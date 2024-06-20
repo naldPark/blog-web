bcd0a649b1157e17ea646370521ea34965d48aea
naldPark <81441317+naldPark@users.noreply.github.com>
2024-06-20 09:13:25 +0900

    1

---
+++ src/pages/wonderwall/sandbox/SandboxPage.vue
@@ -0,0 +1,309 @@
+<template>
+  <div class="sandbox-wrapper">
+    <v-card variant="outlined" elevation="0" class="sandbox-card">
+      <v-card-title class="text-subtitle-1 text-primary">{{
+        $t('sandbox.funcInfo')
+      }}</v-card-title>
+      <v-card-text class="mb-3">
+        <p class="subtitle-2 text-white mt-3 mb-1">{{ $t('sandbox.rules') }}</p>
+        <h4 class="ec2-content font-weight-light" style="margin-right: 12px">
+          {{ $t('sandbox.rulesDesc') }}
+        </h4>
+        <p class="subtitle-2 text-white mt-3 mb-1">
+          {{ $t('sandbox.howToUse') }}
+        </p>
+        <div class="ec2-content font-weight-light" style="margin-right: 12px">
+          {{ $t('sandbox.howToUseDesc') }}
+        </div>
+      </v-card-text>
+    </v-card>
+    <v-card variant="outlined" elevation="0" class="sandbox-card">
+      <v-card-title class="text-subtitle-1 text-primary">
+        {{ $t('sandbox.environmentInfo') }}
+      </v-card-title>
+      <v-card-text>
+        <p class="subtitle-2 mt-3 mb-1">{{ $t('sandbox.status') }}</p>
+        <v-select
+          style="max-width: 300px"
+          item-text="name"
+          class="input-custom pt-0"
+          v-model="selectedStatusItem"
+          :items="statusItems"
+          @change="onChangeStatus"
+        >
+          <template v-slot:selection="item">
+            <span>
+              <v-icon :style="{ color: item.item.value.color }">{{
+                item.item.value.icon
+              }}</v-icon>
+              {{ item.item.value.name }}
+            </span>
+          </template>
+          <template v-slot:item="{ item }">
+            <span>
+              <v-icon :style="{ color: item.value.color }">{{
+                item.value.icon
+              }}</v-icon>
+              {{ item.value.name }}
+            </span>
+          </template>
+        </v-select>
+        <v-row class="mt-3 mb-1 align-center">
+          <v-col cols="12" sm="6">
+            <p class="subtitle-2 text-white mt-3 mb-1">
+              {{ $t('sandbox.instanceInfo') }}
+            </p>
+            <span class="ec2-content">
+              <v-icon class="text-primary mr-1">mdi-ubuntu</v-icon>
+              Linux
+            </span>
+            <v-btn color="error" rounded small @click="accessEndpoint">{{
+              $t('sandbox.startInstance')
+            }}</v-btn>
+          </v-col>
+          <v-col cols="12" sm="6">
+            <p class="subtitle-2 text-white mt-3 mb-1">
+              {{ $t('sandbox.instancePwd') }}
+            </p>
+            <span class="ec2-content" v-if="!showPwd">
+              <v-icon class="text--grey">mdi-eye-off-outline</v-icon>
+              ******
+            </span>
+            <span class="ec2-content" v-else @click="copyClipboard">
+              <v-icon class="text--grey">mdi-eye-outline</v-icon>
+              qwerty1234
+            </span>
+            <v-btn color="background" rounded small @click="showPwd = !showPwd">
+              {{ showPwd ? $t('hide') : $t('show') }}
+            </v-btn>
+          </v-col>
+        </v-row>
+        <p class="subtitle-2 text-white mt-3 mb-1">
+          {{ $t('sandbox.containerImage') }}
+        </p>
+        <span class="ec2-content" style="margin-right: 12px"
+          >/node:16.14.0</span
+        >
+      </v-card-text>
+    </v-card>
+    <v-card variant="outlined" elevation="0" class="sandbox-card">
+      <v-card-title class="text-subtitle-1 text-primary">
+        {{ $t('sandbox.sourceFolder') }}
+      </v-card-title>
+      <v-card-text>
+        <v-treeview
+          v-model="selectedTree"
+          class="sourceFolder"
+          :opened="initiallyOpen"
+          item-key="name"
+          :items="treeItems"
+          activatable
+          open-on-click
+        >
+          <template v-slot:prepend="{ item }">
+            <!-- {{ JSON.stringify(item) }} -->
+            <v-icon v-if="!item.file">
+              {{ 'mdi-folder' }}
+            </v-icon>
+            <v-icon v-else>
+              {{ files[item.file] }}
+            </v-icon>
+          </template>
+        </v-treeview>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { ref, computed } from 'vue';
+  import { useRouter } from 'vue-router';
+  import { useI18n } from 'vue-i18n';
+  import { useAppStatusStore } from '@/store/appStatusStore';
+  import infraService from '@/api/infraService';
+
+  interface StatusItems {
+    name: string;
+    icon: string;
+    color: string;
+  }
+  const appStatusStore = useAppStatusStore();
+  const { t } = useI18n();
+
+  // Router
+  const router = useRouter();
+
+  // State variables
+  const initiallyOpen = ref(['src']);
+  const showPwd = ref(false);
+  const selectedTree = ref<any[]>([]);
+  const selectedStatusItem = ref<any>({
+    name: 'Running',
+    icon: 'mdi-play',
+    color: 'orange',
+  });
+
+  // Files and tree items data
+  const files: any = {
+    html: 'mdi-language-html5',
+    js: 'mdi-nodejs',
+    ts: 'mdi-language-typescript',
+    json: 'mdi-code-json',
+    md: 'mdi-language-markdown',
+  };
+
+  // Status items data
+  const statusItems = ref<StatusItems[]>([
+    {
+      name: 'Running',
+      icon: 'mdi-play',
+      color: 'orange',
+    },
+    {
+      name: 'Stop',
+      icon: 'mdi-stop',
+      color: '#FF0000',
+    },
+    {
+      name: 'Start',
+      icon: 'mdi-play',
+      color: '#2391FF',
+    },
+    {
+      name: 'Waiting',
+      icon: 'mdi-circle-outline',
+      color: '#FF9500',
+    },
+    {
+      name: 'Stopping',
+      icon: 'mdi-circle-outline',
+      color: '#FF0000',
+    },
+    {
+      name: 'Unknown',
+      icon: 'mdi-circle-outline',
+      color: '#868E96',
+    },
+  ]);
+
+  const treeItems = ref<any[]>([
+    {
+      title: 'Dockerfile',
+    },
+    {
+      title: 'node_modules',
+    },
+    {
+      title: 'dist',
+      children: [
+        {
+          title: 'Pty.js',
+          file: 'js',
+        },
+        {
+          title: 'Socket.js',
+          file: 'js',
+        },
+        {
+          title: 'Pty.js',
+          file: 'js',
+        },
+      ],
+    },
+    {
+      title: 'src',
+      children: [
+        {
+          title: 'Pty.ts',
+          file: 'ts',
+        },
+        {
+          title: 'Socket.ts',
+          file: 'ts',
+        },
+        {
+          title: 'Pty.ts',
+          file: 'ts',
+        },
+      ],
+    },
+    {
+      title: 'package-lock.json',
+      file: 'json',
+    },
+    {
+      title: 'tsconfig.json',
+      file: 'json',
+    },
+    {
+      title: 'package.json',
+      file: 'json',
+    },
+  ]);
+
+  // Computed properties
+  const sandboxMessage = computed(() => ({
+    howToUseDesc: t('sandbox.howToUseDesc'),
+    rulesDesc: t('sandbox.rulesDesc'),
+  }));
+
+  // Methods
+  const copyClipboard = () => {
+    const text = 'qwerty1234';
+    const textarea = document.createElement('textarea');
+    document.body.appendChild(textarea);
+    textarea.value = text;
+    textarea.select();
+    textarea.focus();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+    appStatusStore.addToastMessage({
+      type: 'success',
+      message: `${t('copied')}`,
+      buttonMsg: null,
+      timeout: null,
+      buttonCallback: null,
+    });
+  };
+
+  const onChangeStatus = (e: any) => {
+    console.log('e는 ', e);
+    if (e === 'Start') {
+      infraService.getSandboxAccessPoint().then((res: any) => {
+        console.log(res);
+      });
+    } else if (e === 'Stop') {
+      console.log('스탑');
+    }
+  };
+
+  const accessEndpoint = () => {
+    router
+      .push({
+        name: 'SandboxTerminalPage',
+      })
+      .catch((err) => err);
+  };
+</script>
+
+<style lang="scss" scoped>
+  .sandbox-wrapper {
+    height: 100%;
+
+    .sandbox-card {
+      color: darkgray;
+      margin-top: 10px;
+      background: transparent;
+
+      .sourceFolder {
+        font-size: 9pt !important;
+      }
+
+      .ec2-content {
+        margin-left: 10px;
+        margin-right: 12px;
+        color: rgb(134, 134, 134) !important;
+      }
+    }
+  }
+</style>
---
+++ src/pages/wonderwall/streaming/StreamingPage.vue
@@ -0,0 +1,273 @@
+<template>
+  <div>
+    <div v-if="!lazyShow">
+      <v-card class="mx-auto pa-5" color="#121212">
+        <v-row class="justify-align-center">
+          <v-col class="d-flex pa-1" cols="12" sm="4">
+            <v-select class="input-custom" item-title="label" item-value="value" v-model="searchCategory" solo
+              :items="categories" filled :label="$t('category')"></v-select>
+          </v-col>
+          <v-col class="d-flex pa-1" cols="12" sm="8">
+            <v-text-field class="input-custom" solo :label="$t('searchPlaceHolder')" append-icon="mdi-magnify"
+              v-model="searchText" @keyup.enter="searchVideo" @click:append="searchVideo"></v-text-field>
+          </v-col>
+        </v-row>
+      </v-card>
+      <div id="vue-core-video-player-box" class="movie-player text-center" :preload="'metadata'"
+        style="vertical-align: middle">
+        <VideoPlayer :hlsSource='currentMovie.fileSrc' :posterUrl='currentMovie.fileCover'
+          :vttSrc='currentMovie.vttSrc' />
+        <Subtitles />
+      </div>
+      <div class="movie-detail pl-3">
+        <v-chip small color="grey" veriant='outlined' class="mb-2">
+          {{ currentMovie.fileType }}
+        </v-chip>
+        <v-row>
+          <v-col cols="12" sm="8">
+            <h1 class="title">{{ currentMovie.fileName }}</h1>
+            <div class="body-2">
+              {{ $t('registDt') }}: {{ currentMovie.createdDt }}
+            </div>
+          </v-col>
+          <v-col cols="12" sm="4">
+            <div class="download mb-5">
+              <v-btn color="primary" rounded variant="outlined" :disabled="currentMovie.downloadable === 'N'"
+                @click="clickToDownload()">
+                <v-icon>mdi-download</v-icon>
+                {{ $t('video.download') }}
+              </v-btn>
+            </div>
+          </v-col>
+        </v-row>
+        <v-divider class="mt-4 mb-4"></v-divider>
+        <div class="title">
+          {{ $t('video.synopsis') }}
+        </div>
+        <div class="font-weight-light subtitle-2 description text-grey pa-3">{{ currentMovie.fileDesc }}</div>
+      </div>
+      <v-divider class="mt-2 mb-4"></v-divider>
+      <div class="movie-list pa-2">
+        <div class="title">
+          {{ $t('video.recommendedMovie') }}
+        </div>
+        <movie-item v-for="movie in movieList" :key="movie.storageId" :item="movie"
+          @selectMovie="onClickMovie(movie.storageId)" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watch, onMounted, nextTick } from 'vue';
+import { useRouter } from 'vue-router';
+import StorageService from "@/api/storageService";
+import MovieItem from "@/components/wonderwall/video/MovieItem.vue";
+import Config from "@/config";
+import { useDisplay } from 'vuetify';
+import { useI18n } from 'vue-i18n';
+import VideoPlayer from '@/components/wonderwall/video/VideoPlayer.vue';
+import Subtitles from '@/components/wonderwall/video/Subtitles.vue';
+import { useAppStatusStore } from '@/store/appStatusStore';
+const playerOptions = ref({
+  autoplay: false,
+  controls: true,
+  playbackRates: [0.5, 0.75, 1, 1.25, 1.5],
+  preload: "auto",
+  poster: "",
+  controlBar: {
+    playToggle: true,
+    pictureInPictureToggle: true,
+    remainingTimeDisplay: true,
+    progressControl: true,
+    qualitySelector: true,
+  },
+});
+
+const { t } = useI18n();
+
+const categories = [
+  { label: t("all"), value: "" },
+  { label: t("movie"), value: "movie" },
+  { label: t("personal"), value: "personal" },
+  { label: t("etc"), value: "etc" },
+];
+const searchCategory = ref("");
+
+const videoPlayer: any = ref(null);
+const currentMovie: any = ref(null);
+const movieList: any = ref([]);
+const searchText: Ref<string> = ref("");
+const lazyShow = ref(true);
+const display = useDisplay();
+const isMobile: Ref<boolean> = display.smAndDown;
+const appSize = ref(null);
+
+const router = useRouter();
+const appStatusStore = useAppStatusStore();
+
+function player() {
+  return videoPlayer.value.player;
+}
+
+function onPlayerPlay(player: any) {
+  // console.log("온플레이어플레이", player);
+}
+
+function onPlayerReady(player: any) {
+  // console.log("온플레이어레디", player);
+  player().play;
+}
+
+function playVideo(movieSrc: any, vttSrc: any) {
+  const video = {
+    withCredentials: true,
+    type: "application/x-mpegurl",
+    src: movieSrc,
+  };
+  player().reset();
+  player().addRemoteTextTrack(
+    {
+      kind: "subtitles",
+      language: "ko",
+      label: "Korean",
+      src: vttSrc,
+      default: true,
+      mode: "showing",
+    },
+    true
+  );
+  player().src(video);
+  const tracks = player().remoteTextTracks();
+  // console.log("날드!!!", tracks.length); // print out greater than 0
+  player().play;
+}
+
+function loaded(data: any, event: any) {
+  // console.log("로디드", data);
+}
+
+function searchVideo() {
+  router
+    .push({
+      name: "StreamingListPage",
+      params: {
+        searchText: searchText.value,
+        searchCategory: searchCategory.value,
+      },
+    })
+    .catch((err) => err);
+}
+
+async function onClickMovie(storageId: any) {
+  router
+    .replace({
+      name: "StreamingPage",
+      query: {
+        movieId: storageId,
+      },
+    })
+    .catch((err) => err);
+
+  appStatusStore.showLoading();
+  const fileId = storageId;
+  lazyShow.value = true;
+  await StorageService.getVideoDetail(fileId).then(async (res: any) => {
+    let vttSrc = "";
+    if (res.data.data.vttSrc) {
+      await StorageService.videoVtt(fileId).then((res: any) => {
+        vttSrc = URL.createObjectURL(
+          new Blob([res.data], { type: "text/vtt;charset=utf-8;" })
+        );
+      });
+    }
+    const videoSrc = `/api/storage${res.data.data.fileSrc}`;
+    console.log('videoSrc', videoSrc)
+    currentMovie.value = {
+      ...res.data.data, fileSrc: videoSrc, vttSrc: vttSrc
+    }
+    lazyShow.value = false;  // 비디오 컴포넌트 먼저 만들고
+    console.log('22222')
+    // nextTick(() => {
+    //   playVideo(videoSrc, vttSrc); // 컴포넌트 플레이하라고 보냄
+    // });
+    fetchVideoList();
+    console.log('3333', currentMovie.value)
+    appStatusStore.hideLoading();
+  });
+
+}
+
+async function clickToDownload() {
+  appStatusStore.showLoading();
+  await StorageService.download(currentMovie.value.storageId).then((res: any) => {
+    const blob = new Blob([res.data]);
+    const link = document.createElement("a");
+    link.href = window.URL.createObjectURL(blob);
+    link.target = "_self";
+    link.download = `${currentMovie.value.fileName}.mp4`;
+    link.click();
+  });
+  appStatusStore.hideLoading();
+}
+
+function fetchVideoList() {
+  return new Promise((resolve) => {
+    const param: any = {
+      limit: 4,
+      offset: 0,
+      sort: 'random'
+    }
+    StorageService.getVideoList(param).then((response: any) => {
+      if (response.status == 200) {
+        movieList.value = response.data.list;
+      } else {
+        appStatusStore.hideLoading();
+        appStatusStore.addToastMessage({
+          type: 'error',
+          message: response.data.data || response.data.message,
+          buttonMsg: null,
+          timeout: null,
+          buttonCallback: null
+        });
+      }
+      resolve({ finish: true });
+    }).catch((error) => {
+      resolve({ finish: true });
+    });
+  });
+}
+
+onMounted(() => {
+  lazyShow.value = true;
+  appStatusStore.showLoading();
+  // onChangeSize();
+  fetchVideoList();
+  appStatusStore.hideLoading();
+  if (!currentMovie.value) {
+    onClickMovie(Number(router.currentRoute.value.query.movieId));
+  } else {
+    lazyShow.value = false;
+  }
+  console.log('currentMovie', currentMovie)
+});
+
+</script>
+
+<style lang="scss" scoped>
+.movie-player {
+  position: relative;
+  background-color: #000;
+  font-weight: normal;
+  z-index: 2;
+}
+
+.movie-detail {
+  padding-top: 20px;
+
+  .download {
+    padding-right: 20px;
+    text-align: right;
+  }
+}
+</style>
